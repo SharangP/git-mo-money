6fde80b575d177ab810e196dd9c6163c17e1d3a4
Robert Gruener <rjgruener@gmail.com>
2014-02-09 02:44:02 -0500

    db stuff

--- GitMoMoney/config.py
+++ GitMoMoney/config.py
@@ -3,4 +3,4 @@ import os
 DEBUG = True
 GITHUB_KEY = '1f2a8e7b60a6f2b2e3f6d63a91da42b97ed9735e'
 
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://databaseuser:Password@localhost/database'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
