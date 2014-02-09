cfe606d4343a6bd731358dc876c1185b00d61830
Robert Gruener <rjgruener@gmail.com>
2014-02-09 07:17:17 -0500

    update

--- GitMoMoney/forms.py
+++ GitMoMoney/forms.py
@@ -1,7 +1,30 @@
 from flask.ext.wtf import Form
-from wtforms import (BooleanField, TextField, PasswordField, IntegerField,
-                            SelectField, validators, HiddenField)
+from wtforms import (TextField, IntegerField, DecimalField, validators,
+        SelectMultipleField, widgets)
 
 class RepositoryForm(Form):
     repo_owner = TextField('Repository Owner', [validators.Length(min=1, max=35)])
     repo_name = TextField('Repository Name', [validators.Length(min=1, max=35)])
+
+class MultiCheckboxField(SelectMultipleField):
+    """
+    A multiple-select, except displays a list of checkboxes.
+
+    Iterating the field will produce subfields, allowing custom rendering of
+    the enclosed checkbox fields.
+    """
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
+class CollaboratorsForm(Form):
+    collaborators = MultiCheckboxField('Collaborators', coerce=int)
+
+class OptionsForm(Form):
+    max_money = DecimalField('Max Amount of Money To Pay')
+    num_commits = IntegerField('Number Commits Threshold')
+    num_lines = IntegerField('Number Lines Threshold')
+
+    @classmethod
+    def append_field(cls, name, field):
+        setattr(cls, name, field)
+        return cls
--- GitMoMoney/views.py
+++ GitMoMoney/views.py
@@ -1,8 +1,9 @@
 from GitMoMoney import app
 from flask import render_template, flash, redirect, session, url_for, request, g
 from models import User
-from forms import RepositoryForm
-from github import get_repo, get_repo_collaborators, get_repo_commits
+from forms import RepositoryForm, CollaboratorsForm, OptionsForm
+from github import get_repo, get_repo_collaborators, get_repo_commits, get_commit_data
+from wtforms import TextField
 
 @app.route('/')
 def index():
@@ -10,7 +11,7 @@ def index():
 
 @app.route('/get_started', methods=["GET", "POST"])
 def get_started():
-    form = RepositoryForm()
+    form = RepositoryForm(request.form)
     repo_error = False
     if request.method == 'POST' and form.validate():
         try:
@@ -21,14 +22,51 @@ def get_started():
             repo_error = True
     return render_template('get_started.html', form=form, repo_error=repo_error)
 
-@app.route('/collaborators', methods=["GET", "POST"])
-def collaborators():
+@app.route('/repo_collaborators', methods=["GET", "POST"])
+def repo_collaborators():
     if not 'repo' in session:
         return render_template('error.html')
     try:
+        form = CollaboratorsForm(request.form)
+        if request.method == 'POST' and 'collaborators' in session:
+            session['collab_data'] = {}
+            for collab in form.collaborators.data:
+                session['collab_data'][session['collaborators'][collab]['login']] = session['collaborators'][collab]
+            return redirect(url_for('options'))
         repo = session['repo']
         collaborators = get_repo_collaborators(repo['owner']['login'], repo['name'])
-        return render_template('collaborators.html', collaborators=collaborators)
+        form.collaborators.choices = []
+        for (i, collaborator) in enumerate(collaborators):
+            form.collaborators.choices.append((i, collaborator['login']))
+        session['collaborators'] = collaborators
+        return render_template('collaborators.html', form=form, collaborators=collaborators)
     except:
         return render_template('error.html')
 
+@app.route('/options', methods=["GET", "POST"])
+def options():
+    if not 'collaborators' in session or not 'collab_data' in session:
+        return render_template('error.html')
+    class F(OptionsForm):
+        pass
+    for collab in session['collab_data'].keys():
+        setattr(F, collab, TextField("%s's Venmo Key" % collab))
+    form = F(request.form)
+    if request.method == 'POST' and form.validate():
+        try:
+            session['options'] = {}
+            session['options']['max_money'] = form.max_money.data
+            session['options']['num_commits'] = form.num_commits.data
+            session['options']['num_lines'] = form.num_lines.data
+            for collab in session['collab_data'].keys():
+                session['collab_data'][collab]['venmo'] = getattr(form, collab)
+            return redirect(url_for('results'))
+        except:
+            return render_template('error.html')
+    return render_template('options.html', form=form, collaborators=session['collab_data'].keys())
+
+@app.route('/results')
+def results():
+    if not 'repo' in session or not 'collab_data' in session:
+        return render_template('error.html')
+    git_results = get_commit_data(session['repo']['owner']['login'], session['repo']['name'], session['collab_data'].keys())
