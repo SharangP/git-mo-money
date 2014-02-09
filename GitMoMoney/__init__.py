0eb048d6b5a515a1c5341489ef79e61e2fc18cf7
Robert Gruener <rjgruener@gmail.com>
2014-02-09 01:35:02 -0500

    updated for postgres

--- GitMoMoney/__init__.py
+++ GitMoMoney/__init__.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 #from database import Database
 #from flask.ext.login import LoginManager
 
 app = Flask('GitMoMoney')
+app.config.from_pyfile("config.py")
+db = SQLAlchemy(app)
 #login_manager = LoginManager()
 #login_manager.setup_app(app)
-#database = Database(app)
 
 import GitMoMoney.views
