e50f00d2dc49ac66d5db492f2b292d3658bafb05
Robert Gruener <rjgruener@gmail.com>
2014-02-19 16:16:40 -0500

    update config

--- GitMoMoney/config.py
+++ GitMoMoney/config.py
@@ -1,7 +1,7 @@
 import os
 
 DEBUG = True
-GITHUB_KEY = '1f2a8e7b60a6f2b2e3f6d63a91da42b97ed9735e'
+GITHUB_KEY = '346e8e420f7c16edd95d5d6cbc7a50f9fb8ab49e'
 basedir = os.path.abspath(os.path.dirname(__file__))
 SECRET_KEY = 'secret_key'
 
